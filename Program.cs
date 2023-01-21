c3e6b40eb64ce5d963cb72421c8a8db0b9047677
VidyanandSawai17 <vidyanandsawai17@gmail.com>
2023-01-21 20:44:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            employe emp = new employe(1, "Vidyanand", 100000, 200);
+            emp.salary();
+            Console.WriteLine(emp.Display());
+        }
+    }
+}
+
+
+/*    In the existing Emp class accept empid, empname from basic salary from the constructor
+    
+        Calculate salary as below
+
+        HRA 40 % of basic
+        Da  20 % of basic
+        Pf  12 % of basic
+        PT =200
+
+        Calculate the total & net paid salary
+        Basic +hra+da → total
+        Total - (pf+pt)  → net paid salary */
---
+++ employe.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7
+{
+
+    
+
+    public class employe  // variables, constructor,method,property,indexer,event
+    {
+        public int id;
+        public string name;
+        public double sal, hra, da, pf, pt,total, gross;
+
+
+        public employe () //constructor
+        {
+            id = 0;
+            name = "";
+            sal= 0;
+            pt = 200;
+            
+        }
+
+        public  employe(int id, string name, double sal, double pt)  
+        {
+            this.id = id;           // this keyword points to the data member
+            this.name = name;
+            this.sal = sal;
+            this.pt = pt;
+            
+        }
+
+        public void salary()
+        {
+            hra = sal * 0.40;
+            da = sal * 0.20;
+            pf = sal * 0.12;
+            total = hra+ da + pf + pt ;
+            gross = sal - total;
+
+        }
+
+        public string Display()
+        {
+            return $"\n Roll No : {id} \n Name : {name} \n Salary = {sal} \n Gross salary = {gross} ";
+        }
+
+    }
+}
